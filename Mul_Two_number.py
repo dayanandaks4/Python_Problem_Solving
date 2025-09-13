fb1a16f43532b9515f4dade950622fecc67215f0
Dayananda  K S <dayanandaks045@gmail.com>
2025-09-13 12:43:20 +0530

    Update Mul_Two_number.py

--- Mul_Two_number.py
+++ Mul_Two_number.py
@@ -1,7 +1,7 @@
-# Diffrence of two number.
-def Mul(num1,num2):
+# Multiplication of two number.
+def mul(num1,num2):
   n1 = num1
   n2 = num2
   Mul = n1 * n2 
   print(f"Diff: {Mul}")
-Mul(10,5)
+mul(10,5)
