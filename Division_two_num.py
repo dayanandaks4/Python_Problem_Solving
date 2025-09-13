ff4d25c0e3e7bb2f2201fb10cbfe596ba9b184da
Dayananda  K S <dayanandaks045@gmail.com>
2025-09-13 12:45:19 +0530

    Update Division_two_num.py

--- Division_two_num.py
+++ Division_two_num.py
@@ -1,7 +1,17 @@
-# Diffrence of two number.
+# Dividing Two number.
+# div two number we get a float output
 def div(num1,num2):
   n1 = num1
   n2 = num2
   div = float(n1 * n2)
   print(f"Diff: {div}")
 div(10,5)
+
+# div two number we get a int output
+def div(num1,num2):
+  n1 = num1
+  n2 = num2
+  div = int(n1 * n2)
+  print(f"Diff: {div}")
+div(10,5)
+
