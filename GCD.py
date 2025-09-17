40c1c85ce4074ea864c9b936d4bd2e364b6a56bd
Dayananda  K S <dayanandaks045@gmail.com>
2025-09-17 23:56:15 +0530

    Create GCD.py

---
+++ GCD.py
@@ -0,0 +1,18 @@
+x = 12
+y = 30
+a=[]
+b=[]
+c=[]
+for i in range(1,x+1):
+  if x%i==0:
+    a.append(i)
+for j in range(1,y+1):
+  if y%j==0:
+    b.append(j)
+for elem in a:
+  if elem in b:
+    c.append(elem)
+sorts =len(c)
+print(c[sorts-1])
+
+
