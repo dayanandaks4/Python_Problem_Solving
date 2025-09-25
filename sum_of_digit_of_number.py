c8c4692c779a8e45c65ac04f0a672054f70ecc83
Daya <dayanandaks045@gmail.com>
2025-09-25 18:15:07 +0530

    okay

---
+++ sum_of_digit_of_number.py
@@ -0,0 +1,12 @@
+# Find the sum of digits of a number.
+def sum_of_digit(): 
+    user_side = int(input("Enter the number:"))
+    conv_str = str(user_side)
+    sum = 0
+    for num in conv_str:
+        conv_int = int(num)
+        sum = sum + conv_int
+    print(f"sum of number :{sum}")
+sum_of_digit()
+        
+
