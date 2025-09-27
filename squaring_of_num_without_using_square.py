fbb4f979f35ae341e567e8319c26cfde04f6dbb4
Daya <dayanandaks045@gmail.com>
2025-09-27 22:02:56 +0530

    new changes again

--- squaring_of_num_without_using_square.py
+++ squaring_of_num_without_using_square.py
@@ -5,3 +5,10 @@ def square(num):
     print(f"square of number is {squareing}")
 square(5) # square of number is 25
 square(8) # square of number is 64
+
+# The use can enter new numbeer and check the square of the number.
+def square():
+    user = int(input("Enter the number:")) # input = 2
+    squareing = user*user
+    print(f"square of number is {squareing}") # output = 4
+square()
