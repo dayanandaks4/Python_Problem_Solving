1221e882bba5d0c211c77ac7fcc45e030805573e
Daya <dayanandaks045@gmail.com>
2025-09-27 21:59:04 +0530

    changes

--- squareing_of_num_without_using_square.py
+++
@@ -1,6 +0,0 @@
-# 36.	Find the square of a number without using **.
-def square(num):
-    squareing = num*num
-    print(f"square of number is {squareing}")
-square(5)
-square(8)
---
+++ squaring_of_num_without_using_square.py
@@ -0,0 +1,7 @@
+# Find the square of a number without using **.
+
+def square(num):
+    squareing = num*num
+    print(f"square of number is {squareing}")
+square(5) # square of number is 25
+square(8) # square of number is 64
