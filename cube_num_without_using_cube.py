ee2eb439310100834e91936c956d521b270d1d67
Dayananda  K S <dayanandaks045@gmail.com>
2025-09-28 12:26:49 +0530

    Update cube_num_without_using_cube.py

--- cube_num_without_using_cube.py
+++ cube_num_without_using_cube.py
@@ -1,6 +1,5 @@
 # Find the cube of a number without using **.
 
-
 def cube(num):
     squareing = num*num*num
     print(f"square of number is {squareing}")
@@ -8,6 +7,7 @@ cube(1) # cube of given number is : 1
 cube(2) # cube of given number is :8
 
 # The user can enter new number and check the cube of the number.
+
 def cube():
     user = int(input("Enter the number:")) # input = 3
     squareing = user*user*user
