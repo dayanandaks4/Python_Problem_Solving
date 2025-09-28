fc0baedd415ae43f2ee33d95c56e049470a4f5ab
Daya <dayanandaks045@gmail.com>
2025-09-28 12:18:04 +0530

    commit

---
+++ cube_num_without_using_cube.py
@@ -0,0 +1,15 @@
+#Find the cube of a number without using **.
+# Find the square of a number without using **.
+
+def cube(num):
+    squareing = num*num*num
+    print(f"square of number is {squareing}")
+cube(1) # cube of given number is : 1
+cube(2) # cube of given number is :8
+
+# The use can enter new numbeer and check the square of the number.
+def cube():
+    user = int(input("Enter the number:")) # input = 3
+    squareing = user*user*user
+    print(f"square of number is {squareing}") # output = 27
+cube()
