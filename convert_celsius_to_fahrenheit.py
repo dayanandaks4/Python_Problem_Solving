8ea214f66d02975d7badcc3b36b9e5df7f9dd25c
Daya <dayanandaks045@gmail.com>
2025-09-29 13:46:04 +0530

    adding

--- convert_celsius_to_fahrenheit.py
+++ convert_celsius_to_fahrenheit.py
@@ -4,6 +4,7 @@ def convert_celsius_to_fahrenheit(c):
     print(f"{c} of Fahrenheit is {int(f)} degree F")
 convert_celsius_to_fahrenheit(25)
 
+# User can enter Run time Data and Convert Celsius to Fahrenheit.
 def convert_celsius_to_fahrenheit():
     c = int(input("Enter the temperature in Celsius: ")) # 25
     f = ((9/5)*c)+32
