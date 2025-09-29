b2ea1405a08dccbf274844024b7a8db55175b2e8
Daya <dayanandaks045@gmail.com>
2025-09-29 13:40:32 +0530

    commit

---
+++ convert_celsius_to_fahrenheit.py
@@ -0,0 +1,6 @@
+# Convert Celsius to Fahrenheit.
+def convert_celsius_to_fahrenheit(c):
+    f = ((9/5)*c)+32
+    print(f"{c} of Fahrenheit is {int(f)} degree F")
+convert_celsius_to_fahrenheit(25)
+
