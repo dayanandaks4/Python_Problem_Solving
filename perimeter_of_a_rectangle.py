2c46b885fc3b52a34a60dafc7f9906e1819dc1e0
Daya <dayanandaks045@gmail.com>
2025-10-07 22:38:00 +0530

    update

--- perimeter_of_a_rectangle.py
+++ perimeter_of_a_rectangle.py
@@ -3,4 +3,6 @@
 def perimeter_of_rectangle(length, breadth):
     return 2 * (length + breadth)
 print(f"Perimeter of rectangle is: {perimeter_of_rectangle(2, 2)}")
+
+# user can enter length and breadth to find the perimeter of rectangle.
 print(f"Perimeter of rectangle is: {perimeter_of_rectangle(int(input('Enter length: ')), int(input('Enter breadth: ')))}")
