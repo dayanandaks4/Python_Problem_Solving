c2104a8b526e98dc5ad84c2e86b6a1f16a09abf9
dayanandaks4 <dayanandaks045@gmail.com>
2025-10-11 23:24:04 +0530

    new cahngews

---
+++ Reverse_string.py
@@ -0,0 +1,9 @@
+def reverse_string(s):
+    """
+    This function takes a string as input and returns the string reversed.
+    
+    :param s: str - The string to be reversed
+    :return: str - The reversed string
+    """
+    return s[::-1]
+print(reverse_string("Hello, World!"))  # Output: !dlroW ,olleH
