2c7e0059ce5a5a3fb97a80f1936e532dc78e903c
dayanandaks4 <dayanandaks045@gmail.com>
2025-10-14 23:57:41 +0530

    new cahanges

--- capitalize_the_first_word.py
+++ capitalize_the_first_word.py
@@ -1,4 +1,5 @@
-# 60.	Capitalize the first letter of each word in a string
+# Capitalize the first letter of each word in a string
 def capitalize_first_word(s):
     return s.title()
 print(capitalize_first_word("hello world"))  # Output: "Hello World"
+print(capitalize_first_word(input("Enter a string: ")))
